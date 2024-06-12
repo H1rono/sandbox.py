a509bcbff22ed37593aa5a8a000a9edcda0539a6
H1rono <hronok66@gmail.com>
2024-06-12 13:04:43 +0900

    :sparkles: Impl `GET /hello/{name}`

--- src/aiohttp_sand/__init__.py
+++ src/aiohttp_sand/__init__.py
@@ -1,11 +1,18 @@
 from aiohttp import web
 
 
+# GET /ping
 async def ping(_handle: web.Request) -> web.Response:
     return web.Response(text="pong")
 
 
+# GET /hello/{name}
+async def hello(req: web.Request) -> web.Response:
+    name = req.match_info.get("name")
+    return web.Response(text=f"Hello, {name}!")
+
+
 def serve() -> None:
     app = web.Application()
-    app.add_routes([web.get("/ping", ping)])
+    app.add_routes([web.get("/ping", ping), web.get("/hello/{name}", hello)])
     web.run_app(app)
