adf5b35967a5858617c0ac7a9915d6ebc4725f1c
H1rono <hronok66@gmail.com>
2024-06-13 18:36:58 +0900

    :sparkles: Load postgres configuration from file

--- src/polls/__init__.py
+++ src/polls/__init__.py
@@ -1,9 +1,10 @@
 from aiohttp import web
 
-from .app import setup_routes
+from .app import load_config, setup_routes
 
 
 def main() -> None:
     app = web.Application()
     setup_routes(app)
+    load_config(app)
     web.run_app(app)
--- src/polls/app.py
+++ src/polls/app.py
@@ -1,8 +1,24 @@
+from pathlib import Path
+from typing import Any
+
+import yaml
 from aiohttp import web
 
 
 async def index(request: web.Request) -> web.Response:
     return web.Response(text="Hello, aiohttp!")
+
+
+def get_config(path: str | None = None) -> Any:
+    path = path or str(Path(__file__).parent / "config.yml")
+    with open(path) as f:
+        config = yaml.safe_load(f)
+    return config
+
+
+def load_config(app: web.Application, path: str | None = None) -> None:
+    config = get_config(path)
+    app["config"] = config
 
 
 def setup_routes(app: web.Application) -> None:
