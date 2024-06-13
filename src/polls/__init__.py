83c9d16ba55bc3eb907267a0156be5503c5eadba
H1rono <hronok66@gmail.com>
2024-06-13 19:55:54 +0900

    :sparkles: Add init-db script

--- src/polls/__init__.py
+++ src/polls/__init__.py
@@ -1,6 +1,16 @@
 from aiohttp import web
+from sqlalchemy import create_engine
 
-from .app import load_config, setup_routes
+from . import db
+from .app import get_config, load_config, setup_routes
+
+
+def init_db() -> None:
+    config = get_config()
+    db_url = db.DSN.format(**config["postgres"])
+    engine = create_engine(db_url)
+    db.create_tables(engine)
+    db.create_sample_data(engine)
 
 
 def main() -> None:
