6de1478aa10f42ddde42a019a68060b867dd16d8
H1rono <hronok66@gmail.com>
2024-07-27 15:24:31 +0900

    :recycle: Logging in sinwave

--- src/plot/live.py
+++ src/plot/live.py
@@ -1,3 +1,5 @@
+import logging
+import sys
 from collections import deque
 
 import matplotlib.pyplot as plt
@@ -7,6 +9,8 @@ from matplotlib.axes import Axes
 from matplotlib.figure import Figure
 from typing_extensions import Generator, NoReturn
 
+_logger = logging.getLogger(__name__)
+
 
 class SinAnimationState:
     def __init__(self, fig: Figure) -> None:
@@ -14,30 +18,36 @@ class SinAnimationState:
         self.xdata = deque[float]()
         self.ydata = deque[float]()
         self.line, *_ = self.ax.plot(self.xdata, self.ydata, "ro")
+        self.logger = _logger.getChild(self.__class__.__name__)
 
     def init(self) -> tuple[Axes]:
         self.ax.set_xlim(0, 2 * np.pi)
         self.ax.set_ylim(-1.2, 1.2)
+        self.logger.debug("init")
         return (self.ax,)
 
     def update(self, frame: float) -> tuple[Axes]:
-        self.xdata.append(frame)
-        self.ydata.append(np.sin(frame))
-        if frame > 2 * np.pi:
+        x, y = frame, np.sin(frame)
+        self.logger.debug(f"update {x=}, {y=}")
+        self.xdata.append(x)
+        self.ydata.append(y)
+        if x > 2 * np.pi:
             xleft = self.xdata.popleft()
             self.ydata.popleft()
-            self.ax.set_xlim(xleft, frame)
+            self.ax.set_xlim(xleft, x)
         self.line.set_data(self.xdata, self.ydata)
         return (self.ax,)
 
     def frames(self) -> Generator[float, None, NoReturn]:
         v = 0.0
         while True:
+            self.logger.debug("generate frame")
             yield v
             v += 0.1
 
 
 def sinwave() -> None:
+    logging.basicConfig(stream=sys.stderr, level=logging.INFO)
     fig = plt.figure()
     state = SinAnimationState(fig)
     _ani = FuncAnimation(
